6814fe5409cc81b71ff29e88ee6954fb0460246b
Pierre Jecrois <jecroisp@wit.edu>
2023-12-10 23:52:34 -0500

    Q3

---
+++ Questions/Question3.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Load the datasets
+all_seasons_df = pd.read_csv(r'all_seasons.csv')
+team_totals_df = pd.read_csv(r'Team Totals.csv')
+team_stats_per_game_df = pd.read_csv(r'Team Stats Per Game.csv')
+
+
+
+relevant_columns = ['pts', 'ast', 'reb', 'pts_playoffs', 'ast_playoffs', 'reb_playoffs']
+
+
+all_seasons_df['pts_playoffs'] = all_seasons_df['pts'].shift(-1)
+all_seasons_df['ast_playoffs'] = all_seasons_df['ast'].shift(-1)
+all_seasons_df['reb_playoffs'] = all_seasons_df['reb'].shift(-1)
+
+
+correlation_matrix = all_seasons_df[relevant_columns].corr()
+
+# Plotting the correlation heatmap
+plt.figure(figsize=(10, 8))
+heatmap = sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', square=True)
+plt.title('Correlation between Regular Season and Playoff Performance')
+plt.show()
